c4fd28a2f4b45015892e9d282e98a4e6c430a413
tobiStrings <ligalititobiloluwa2005@gmail.com>
2022-06-14 15:38:14 +0100

    First commit

---
+++ src/main/java/com/example/musicalinstrumentstore/MusicalInstrumentStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.musicalinstrumentstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicalInstrumentStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicalInstrumentStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/musicalinstrumentstore/MusicalInstrumentStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.musicalinstrumentstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicalInstrumentStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
